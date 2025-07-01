fd6005e9abf2f25d37da67f62d31d63704bdbe75
Ivan <magasalliwan@gmail.com>
2025-07-01 16:47:57 +0300

    Homework_2

--- Homework_2.py
+++ Homework_2.py
@@ -1,1 +1,12 @@
-print('Hello World!')
+from random import sample
+
+
+def get_numbers_ticket(min, max, quantity):
+
+    if not (1 <= min <= max <= 1000) or not (min <= quantity <= max):
+        return []
+    else:
+        print(sorted(sample(range(min, max+1), quantity)))
+
+
+get_numbers_ticket(1, 49, 6)
