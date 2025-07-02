9bb45228d212f84ec96a4884debfeebf94435454
Ivan <magasalliwan@gmail.com>
2025-07-02 13:53:08 +0300

    Bug fixed

--- Homework_1.py
+++ Homework_1.py
@@ -1,7 +1,11 @@
 from datetime import datetime, timedelta
 
+
 def get_days_from_today(date):
-    date_string = datetime.strptime(date, "%Y-%m-%d").date()
-    now = datetime.today().date()
-    difference = now - date_string
-    return difference.days
+    try:
+        date_string = datetime.strptime(date, "%Y-%m-%d").date()
+        now = datetime.today().date()
+        difference = now - date_string
+        return difference.days
+    except ValueError:
+        print(f'Please enter a valid date: ')
