b526a9450a80123b36c4defb5592a6c6b16f83f9
Ivan <magasalliwan@gmail.com>
2025-07-04 21:10:49 +0300

    Module 4 First task

--- Homework_2.py
+++ Homework_2.py
@@ -1,9 +1,27 @@
-from random import sample
+from pathlib import Path
+
+data = ['Alex Korp,3000', 'Nikita Borisenko,2000', 'Sitarama Raju,1000']
+with open('Work/employee_data.txt', 'w', encoding='utf-8') as file:
+    for line in data:
+        file.write(f'{line}\n')
+
+path = Path('Work/employee_data.txt')
 
 
-def get_numbers_ticket(min, max, quantity):
+def total_salary(path):
+    try:
+        salaries = []
+        with open(path, 'r', encoding='utf-8') as file:
+            for line in file:
+                name, salary = line.strip().split(',')
+                salaries.append(int(salary))
+            total = sum(salaries)
+            average = int(total / len(salaries))
+            print(
+                f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
 
-    if not (1 <= min <= max <= 1000) or not (quantity <= max - min + 1):
-        return []
-    else:
-        return sorted(sample(range(min, max+1), quantity))
+    except Exception as e:
+        print(f'{e} with file')
+
+
+total_salary(path)
